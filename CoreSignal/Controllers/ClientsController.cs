6869d3f44a515772203cd52ddefb63ccac8b0883
westfive <chenlong5469@163.com>
2017-03-22 09:59:37 +0800

    change api

--- CoreSignal/Controllers/ClientsController.cs
+++ CoreSignal/Controllers/ClientsController.cs
@@ -21,30 +21,65 @@ namespace CoreSiganl.Controllers
             return value;
         }
 
-        // GET api/sessions/idindex
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET api/sessions/idindex
+        //[HttpGet("{id}")]
+        //public List<object> Get(int id)
+        //{
+        //    switch (id)
+        //    {
+        //        case 1:
+        //            List<object> valuelist = new List<object>();
+        //            foreach (var item in MessageHub.messageContextList)
+        //            {
+        //                valuelist.Add(item.message_content);
+        //            }
+        //            return valuelist;
+        //        case 2:
+        //            List<object> valuelist2 = new List<object>();
+        //            foreach (var item in MessageHub.messageContextList)
+        //            {
+        //                valuelist2.Add(item.message_content.lane_status);
+        //            }
+        //            return valuelist2;
+        //        case 3:
+        //            List<object> valuelist3 = new List<object>();
+        //            foreach (var item in MessageHub.messageContextList)
+        //            {
+        //                valuelist3.Add(item.message_content.lane_status.progress_bar);
+        //            }
+        //            return valuelist3;
+        //        default:
+        //            List<object> valuelist0 = new List<object>();
+        //            foreach (var item in MessageHub.messageContextList)
+        //            {
+        //                valuelist0.Add(item.message_content);
+        //            }
+        //            return valuelist0;
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
 
-        }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //    }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
