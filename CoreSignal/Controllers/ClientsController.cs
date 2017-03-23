189723c88910c747324c440565b21d1be81dda35
westfive <chenlong5469@163.com>
2017-03-23 10:48:05 +0800

    2测通过

--- CoreSignal/Controllers/ClientsController.cs
+++ CoreSignal/Controllers/ClientsController.cs
@@ -23,13 +23,51 @@ namespace CoreSiganl.Controllers
             return value;
         }
 
-        [HttpPost("{LaneJson}")]
-        public string Post(string LaneJson)
+        [HttpPost("{message_status}")]
+        public string Post([FromBody]Pf_MessageStatus_Obj message_status)
         {
             
             try
             {
-                var temp = JsonHelper.DeserializeJsonToObject<Pf_MessageStatus_Obj>(LaneJson);
+                //var temp = JsonHelper.DeserializeJsonToObject<Pf_MessageStatus_Obj>(LaneJson);
+                //lock (MessageHub.messageContextList)
+                //{
+                //    if (MessageHub.messageContextList.Count(x => x.message_content.lane_id == temp.message_content.lane_id) > 0)
+                //    {
+                //        //var temptt = messageContextList.FirstOrDefault(x => x.message_content.LaneID == temp.message_content.LaneID);
+
+                //        MessageHub.messageContextList[MessageHub.messageContextList.FindIndex(x => x.message_content.lane_id == temp.message_content.lane_id)] = temp;
+
+                //    }
+                //}
+               lock(MessageHub.messageContextList)
+                {
+                    if(MessageHub.messageContextList.Count(x=>x.message_content.lane_id==message_status.message_content.lane_id)>0)
+                    {
+                        MessageHub.messageContextList[MessageHub.messageContextList.FindIndex(x => x.message_content.lane_id == message_status.message_content.lane_id)] = message_status;
+                    }
+                }
+
+                Loger.AddLogText(DateTime.Now.ToString() + "修改:" + message_status.message_content.lane_name + "数据成功");
+                return "修改成功";
+            }
+
+                
+                
+            catch (Exception ex)
+            {
+                Loger.AddErrorText("API修改车道数据失败", ex);
+                return "修改失败"+ex.ToString();
+                
+            }
+        }
+
+        [HttpPost("{message_status_json}")]
+        public string Post(string message_status_json)
+        {
+            try
+            {
+                var temp = JsonHelper.DeserializeJsonToObject<Pf_MessageStatus_Obj>(message_status_json);
                 lock (MessageHub.messageContextList)
                 {
                     if (MessageHub.messageContextList.Count(x => x.message_content.lane_id == temp.message_content.lane_id) > 0)
@@ -40,80 +78,16 @@ namespace CoreSiganl.Controllers
 
                     }
                 }
-
                 Loger.AddLogText(DateTime.Now.ToString() + "修改:" + temp.message_content.lane_name + "数据成功");
                 return "修改成功";
             }
-
-                
-                
             catch (Exception ex)
             {
                 Loger.AddErrorText("API修改车道数据失败", ex);
-                return "修改失败";
-                
+                return "修改失败" + ex.ToString();
             }
         }
 
-        //// GET api/sessions/idindex
-        //[HttpGet("{id}")]
-        //public List<object> Get(int id)
-        //{
-        //    switch (id)
-        //    {
-        //        case 1:
-        //            List<object> valuelist = new List<object>();
-        //            foreach (var item in MessageHub.messageContextList)
-        //            {
-        //                valuelist.Add(item.message_content);
-        //            }
-        //            return valuelist;
-        //        case 2:
-        //            List<object> valuelist2 = new List<object>();
-        //            foreach (var item in MessageHub.messageContextList)
-        //            {
-        //                valuelist2.Add(item.message_content.lane_status);
-        //            }
-        //            return valuelist2;
-        //        case 3:
-        //            List<object> valuelist3 = new List<object>();
-        //            foreach (var item in MessageHub.messageContextList)
-        //            {
-        //                valuelist3.Add(item.message_content.lane_status.progress_bar);
-        //            }
-        //            return valuelist3;
-        //        default:
-        //            List<object> valuelist0 = new List<object>();
-        //            foreach (var item in MessageHub.messageContextList)
-        //            {
-        //                valuelist0.Add(item.message_content);
-        //            }
-        //            return valuelist0;
-
-
-
-        //    }
-
-
-        //}
-
-        //// POST api/values
-        //[HttpPost]
-        //public void Post([FromBody]string value)
-        //{
-
-        //}
-
-        //// PUT api/values/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        //// DELETE api/values/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
+    
     }
 }
